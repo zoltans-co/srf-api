88140bf080e5b67e76c3e0f1dede27f13c27d2ce
Szilagyi Zoltan <mail@zoltans.co>
2023-12-11 19:16:17 +0100

    first commit

---
+++ src/main/java/co/zoltans/srf/srfapi/SrfApiApplication.java
@@ -0,0 +1,13 @@
+package co.zoltans.srf.srfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrfApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/zoltans/srf/srfapi/SrfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.zoltans.srf.srfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
